af0812538fd4f3889c211f6252d4d7d6530a83b8
kyubuns <kyubuns@gmail.com>
2018-02-19 20:23:52 +0900

    feat: 弾き返すアニメ微調整

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -30,12 +30,12 @@ namespace Game
             spriteRenderer.color = spriteRenderer.color / 2;
             spriteRenderer.sortingLayerID = SortingLayer.NameToID("ReflectedBall");
 
-            Anime.PlayRelative(ball.transform.position, new Vector3(0f, 15f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(1.5f)))
+            Anime.PlayRelative(ball.transform.position, new Vector3(0f, 15f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(2.0f)))
                 .TakeUntilDestroy(ball.gameObject)
                 .DoOnCompleted(() => Destroy(ball.gameObject))
                 .SubscribeToPosition(ball.gameObject);
 
-            Anime.PlayRelative(ball.transform.localScale, new Vector3(-0.2f, 1f, 0f), Easing.OutCubic(TimeSpan.FromSeconds(0.5f)))
+            Anime.PlayRelative(ball.transform.localScale, new Vector3(-0.2f, 1f, 0f), Easing.OutCubic(TimeSpan.FromSeconds(0.75f)))
                 .TakeUntilDestroy(ball.gameObject)
                 .SubscribeToLocalScale(ball.gameObject);
         }
