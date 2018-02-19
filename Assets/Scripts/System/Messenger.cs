eb0167e805a3f74089501accf121d733c5c41037
kyubuns <kyubuns@gmail.com>
2018-02-19 13:39:43 +0900

    feat: Messenger

---
+++ Assets/Scripts/System/Messenger.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Game.System
+{
+    public class Messenger : MonoBehaviour
+    {
+        public static MessageBroker Broker { get; private set; }
+
+        public void Awake()
+        {
+            Assert.IsNull(Broker);
+            Broker = new MessageBroker();
+        }
+
+        public void OnDestroy()
+        {
+            Broker = null;
+        }
+    }
+}
