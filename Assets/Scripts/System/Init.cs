81d293a3f36cdad0711237e3f7e4c70e7ca92b63
kyubuns <kyubuns@gmail.com>
2018-02-19 13:36:48 +0900

    feat: InitScene

---
+++ Assets/Scripts/System/Init.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.System
+{
+    public class Init : MonoBehaviour
+    {
+        public IEnumerator Start()
+        {
+            yield return new WaitForEndOfFrame();
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
