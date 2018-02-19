d393b2f15f75a0b1cde547e7ea79d490d2cb3b6f
kyubuns <kyubuns@gmail.com>
2018-02-19 15:03:53 +0900

    feat: プレイヤーは壁までしか移動出来ない

--- Assets/Scripts/Game/PlayerMover.cs
+++ Assets/Scripts/Game/PlayerMover.cs
@@ -5,15 +5,23 @@ namespace Game
     public class PlayerMover : MonoBehaviour
     {
         private Vector2 originalPosition;
+        private float rangeMin;
+        private float rangeMax;
 
-        public void Awake()
+        public void Start()
         {
             originalPosition = transform.position;
+            var size = GetComponentInChildren<Collider2D>().bounds.size;
+            var leftHit = Physics2D.Raycast(originalPosition, Vector2.left, float.PositiveInfinity, LayerMask.GetMask("Stage"));
+            var rightHit = Physics2D.Raycast(originalPosition, Vector2.right, float.PositiveInfinity, LayerMask.GetMask("Stage"));
+            rangeMin = originalPosition.x - leftHit.distance + size.x / 2.0f;
+            rangeMax = originalPosition.x + rightHit.distance - size.x / 2.0f;
         }
 
         public void Update()
         {
             var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.x = Mathf.Clamp(worldPosition.x, rangeMin, rangeMax);
             worldPosition.y = originalPosition.y;
             worldPosition.z = 0.0f;
             transform.position = worldPosition;
