1b08d3b343e07153ccebbe49627926cf8053f87b
kyubuns <kyubuns@gmail.com>
2018-02-19 13:32:14 +0900

    chore: RiderPluginをgitignoreに追加

---
+++ Assets/Scripts/System/SceneFlow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.System
+{
+    public class SceneFlow : MonoBehaviour
+    {
+        public IEnumerator Start()
+        {
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log("Start");
+        }
+    }
+}
