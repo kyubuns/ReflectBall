8973df585e0e1392309a73ce335834898bbf6584
kyubuns <kyubuns@gmail.com>
2018-02-19 19:28:45 +0900

    feat: ゲージ側の弾き返しアニメーション

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -8,11 +8,23 @@ namespace Game
 {
     public class Player : MonoBehaviour
     {
+        [SerializeField] private SpriteRenderer sprite;
+        private readonly Subject<Unit> subject = new Subject<Unit>();
+
+        public void Start()
+        {
+            subject.Select(_ => Anime.Play(new Vector3(1.5f, 1.5f, 1.5f), new Vector3(1f, 1f, 1f), Easing.OutElastic(TimeSpan.FromSeconds(0.5f))))
+                .Switch()
+                .SubscribeToLocalScale(sprite.transform)
+                .AddTo(this);
+        }
+
         public void OnHitBall(Ball ball)
         {
             Messenger.Broker.Publish(new OnRefrectBall());
 
             ball.IsAlive = false;
+            subject.OnNext(Unit.Default);
 
             Anime.PlayRelative(ball.transform.position, new Vector3(0f, 15f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(1.5f)))
                 .DoOnCompleted(() => Destroy(ball.gameObject))
