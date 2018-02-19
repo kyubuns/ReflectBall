be311aae84dddcff8caadd5a29b3a3fb0a953ec4
kyubuns <kyubuns@gmail.com>
2018-02-19 14:55:07 +0900

    feat: プレイヤーを動かせる

---
+++ Assets/Scripts/Game/PlayerMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        private Vector2 originalPosition;
+
+        public void Awake()
+        {
+            originalPosition = transform.position;
+        }
+
+        public void Update()
+        {
+            var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.y = originalPosition.y;
+            worldPosition.z = 0.0f;
+            transform.position = worldPosition;
+        }
+    }
+}
