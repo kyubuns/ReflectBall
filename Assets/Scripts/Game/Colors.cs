0f31272d4d1985cdd4e4a7d1470defbc2c078201
kyubuns <kyubuns@gmail.com>
2018-02-20 09:21:38 +0900

    feat: 青が見にくかったので色調整

--- Assets/Scripts/Game/Colors.cs
+++ Assets/Scripts/Game/Colors.cs
@@ -4,7 +4,7 @@ namespace Game
 {
     public static class Colors
     {
-        public static readonly Color Top = new Color32(41, 76, 204, 255);
-        public static readonly Color Bottom = new Color32(236, 82, 82, 255);
+        public static readonly Color Top = new Color32(123, 149, 246, 255);
+        public static readonly Color Bottom = new Color32(246, 123, 123, 255);
     }
 }
