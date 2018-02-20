4c4670ef2de370992e69c15ca8f40012c01d09c2
kyubuns <kyubuns@gmail.com>
2018-02-20 19:27:01 +0900

    feat: バーにボールが当たった時のアニメーション改修

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -10,10 +10,13 @@ namespace Game
     {
         [SerializeField] private SpriteRenderer sprite;
         private readonly Subject<Unit> subject = new Subject<Unit>();
+        private readonly TimeSpan anime1 = TimeSpan.FromSeconds(0.05f);
 
         public void Start()
         {
-            subject.Select(_ => Anime.Play(new Vector3(1.5f, 1.5f, 1.5f), new Vector3(1f, 1f, 1f), Easing.OutElastic(TimeSpan.FromSeconds(0.5f))))
+            subject.Select(_ => Anime
+                    .Play(new Vector3(0.8f, 0.6f, 1f), new Vector3(0.8f, 0.6f, 1f), Easing.Linear(anime1))
+                    .Play(new Vector3(1.5f, 1.5f, 1f), new Vector3(1f, 1f, 1f), Easing.OutElastic(TimeSpan.FromSeconds(0.5f))))
                 .Switch()
                 .SubscribeToLocalScale(sprite.transform)
                 .AddTo(this);
@@ -27,17 +30,26 @@ namespace Game
             subject.OnNext(Unit.Default);
 
             var spriteRenderer = ball.GetComponent<SpriteRenderer>();
-            spriteRenderer.color = spriteRenderer.color / 2;
+            spriteRenderer.color = spriteRenderer.color / 1.5f;
             spriteRenderer.sortingLayerID = SortingLayer.NameToID("ReflectedBall");
 
-            Anime.PlayRelative(ball.transform.position, new Vector3(0f, 15f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(2.0f)))
-                .TakeUntilDestroy(ball.gameObject)
-                .DoOnCompleted(() => Destroy(ball.gameObject))
-                .SubscribeToPosition(ball.gameObject);
+            var s = ball.transform.localScale;
+            s.y /= 2f;
+            ball.transform.localScale = s;
 
-            Anime.PlayRelative(ball.transform.localScale, new Vector3(-0.2f, 1f, 0f), Easing.OutCubic(TimeSpan.FromSeconds(0.75f)))
+            Observable.Timer(anime1)
                 .TakeUntilDestroy(ball.gameObject)
-                .SubscribeToLocalScale(ball.gameObject);
+                .Subscribe(_ =>
+                {
+                    Anime.PlayRelative(ball.transform.position, new Vector3(0f, 30f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(3f)))
+                        .TakeUntilDestroy(ball.gameObject)
+                        .DoOnCompleted(() => Destroy(ball.gameObject))
+                        .SubscribeToPosition(ball.transform);
+
+                    Anime.PlayRelative(ball.transform.localScale, new Vector3(-0.3f, 10f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(0.5f)))
+                        .TakeUntilDestroy(ball.gameObject)
+                        .SubscribeToLocalScale(ball.transform);
+                }).AddTo(ball);
         }
     }
 }
