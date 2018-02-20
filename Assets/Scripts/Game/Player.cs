bd14f5c85e9c816d9ea015cad802eac5053e918a
kyubuns <kyubuns@gmail.com>
2018-02-20 11:17:20 +0900

    Revert "feat: 跳ね返したボールを反射角に飛ばす"
    わかりにくいからやめる

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -30,16 +30,11 @@ namespace Game
             spriteRenderer.color = spriteRenderer.color / 2;
             spriteRenderer.sortingLayerID = SortingLayer.NameToID("ReflectedBall");
 
-            var moveVector2 = ball.Velocity.normalized;
-            moveVector2.y *= -1f;
-            var moveVector3 = new Vector3(moveVector2.x, moveVector2.y, ball.transform.position.z);
-
-            Anime.PlayRelative(ball.transform.position, moveVector3 * 15f, Easing.OutExpo(TimeSpan.FromSeconds(2.0f)))
+            Anime.PlayRelative(ball.transform.position, new Vector3(0f, 15f, 0f), Easing.OutExpo(TimeSpan.FromSeconds(2.0f)))
                 .TakeUntilDestroy(ball.gameObject)
                 .DoOnCompleted(() => Destroy(ball.gameObject))
                 .SubscribeToPosition(ball.gameObject);
 
-            ball.transform.localRotation = Quaternion.FromToRotation(Vector3.up,  moveVector2);
             Anime.PlayRelative(ball.transform.localScale, new Vector3(-0.2f, 1f, 0f), Easing.OutCubic(TimeSpan.FromSeconds(0.75f)))
                 .TakeUntilDestroy(ball.gameObject)
                 .SubscribeToLocalScale(ball.gameObject);
