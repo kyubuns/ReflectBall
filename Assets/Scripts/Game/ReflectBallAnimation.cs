f6f973467a528069acadad30f14c302fdd2761f9
kyubuns <kyubuns@gmail.com>
2018-02-20 09:53:47 +0900

    feat: スコアが入った時に文字も揺れる

---
+++ Assets/Scripts/Game/ReflectBallAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AnimeRx;
+using GameSystem;
+using UniRx;
+using UnityEngine;
+
+namespace Game
+{
+    public class ReflectBallAnimation : MonoBehaviour
+    {
+        [SerializeField] private Vector3 big;
+
+        public void Start()
+        {
+            Messenger.Broker.Receive<OnRefrectBall>().Select(_ => Anime.Play(big, Vector3.one, Easing.OutElastic(TimeSpan.FromSeconds(0.5f))))
+                .TakeUntilDestroy(gameObject)
+                .Switch()
+                .SubscribeToLocalScale(transform);
+        }
+    }
+}
